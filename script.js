ee94c1445ee925d25a0985fbcc593886533fd24f
itsmeacris <itsmeacris17@gmail.com>
2025-08-19 22:49:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -190,14 +190,15 @@ canvas.addEventListener("click", function (e) {
 
   if (gameOver) {
     // Restart button
-    if (
-      clickX >= canvas.width / 2 - 50 &&
-      clickX <= canvas.width / 2 + 50 &&
-      clickY >= canvas.height / 2 + 40 &&
-      clickY <= canvas.height / 2 + 75
-    ) {
-      resetGame();
-    }
+  if (
+  clickX >= canvas.width / 2 - 70 &&
+  clickX <= canvas.width / 2 + 70 &&
+  clickY >= canvas.height / 2 + 50 &&
+  clickY <= canvas.height / 2 + 95
+) {
+  resetGame();
+}
+
   } else {
     bird.flap();
   }
@@ -214,6 +215,7 @@ function resetGame() {
 
 loop();
 const scale = canvas.width / 400; // used to scale text/buttons for all screens
+
 
 
 
